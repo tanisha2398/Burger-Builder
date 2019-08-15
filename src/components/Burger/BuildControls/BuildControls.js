42e6b7c29dd17e24f80c3f27de18152d9d0ae4e8
tanisha2398 <tanishanegipro@gmail.com>
2019-08-15 22:48:02 +0530

    add multinle build control to burger builder

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./BuildControls.css";
+import BuildControl from "./BuildControl/BuildControl";
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Meat", type: "meat" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Bacon", type: "bacon" }
+];
+const buildControls = props => (
+  <div className={classes.BuildControls}>
+    {controls.map(control => (
+      <BuildControl key={control.label} label={control.label} />
+    ))}
+  </div>
+);
+export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 import Aux from "../../higherOrderComponent/Auxilary";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 class BurgerBuilder extends Component {
   // constructor(props){
   //   super(props);
@@ -18,7 +19,7 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </Aux>
     );
   }
