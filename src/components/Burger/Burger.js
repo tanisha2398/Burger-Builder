c7266329210d6c5d96b408b9c1e2399873d9780c
tanisha2398 <tanishanegipro@gmail.com>
2019-08-15 20:24:50 +0530

    add reduce method on array to get sum of ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,21 @@ import React from "react";
 import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    console.log("igKey", igKey);
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      console.log("props.ingredients[igKey],i", props.ingredients[igKey], i);
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      console.log("igKey", igKey);
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        console.log("props.ingredients[igKey],i", props.ingredients[igKey], i);
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please add Ingredient</p>;
+  }
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
