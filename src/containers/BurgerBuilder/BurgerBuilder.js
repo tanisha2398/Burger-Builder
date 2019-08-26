09f1ebcdb072e23b35ec409fad68ee166a873a2e
tanisha2398 <tanishanegipro@gmail.com>
2019-08-26 14:51:46 +0530

    add post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Aux from "../../higherOrderComponent/Auxilary/Auxilary";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -77,7 +78,24 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   };
   purchaseContinueHandler = () => {
-    alert("You Continue");
+    // alert("You Continue");
+    const order={
+      ingredients:this.state.ingredients,
+      price:this.state.totalPrice,
+      customer:{
+        name:'Tanisha',
+        address:{
+          colony:'Anupratap',
+          pincode:'323307',
+          country:'India'
+        },
+        email:'tanishanegipro@gmail.com'
+      },
+      deliveryMethod:'fastest'
+    }
+    axios.post('/orders.json',order)
+      .then(response=>console.log(response))
+      .catch(error=>console.log(error))
   };
   render() {
     const disabledInfo = { ...this.state.ingredients };
