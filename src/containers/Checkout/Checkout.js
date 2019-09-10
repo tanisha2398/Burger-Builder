7742f2493f5230d8654049cb427f00f5c98d1a62
tanisha2398 <tanishanegipro@gmail.com>
2019-09-10 12:17:23 +0530

    navigating to contact data component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import { Route } from "react-router-dom";
+import ContactData from "./ContactData/ContactData";
 class Checkout extends Component {
   state = {
     ingredients: {
@@ -9,13 +11,13 @@ class Checkout extends Component {
       bacon: 1
     }
   };
-  componentDidMount(){
-    const query=new URLSearchParams(this.props.location.search);
-    const ingredients={};
-    for(let param of query.entries()){
-      ingredients[param[0]]=+param[1]
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
     }
-    this.setState({ingredients:ingredients})
+    this.setState({ ingredients: ingredients });
   }
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
@@ -30,6 +32,10 @@ class Checkout extends Component {
           ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.path + "/contact-data"}
+          component={ContactData}
         />
       </div>
     );
