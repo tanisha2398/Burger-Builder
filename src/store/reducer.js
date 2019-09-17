3c892d71874efa7054bac34bc03627169da07efa
tanisha2398 <tanishanegipro@gmail.com>
2019-09-17 23:54:17 +0530

    add redux

---
+++ src/store/reducer.js
@@ -0,0 +1,8 @@
+import * as actionTypes from "./action";
+const initialState = {
+  ingredients: null,
+  totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {};
+export default reducer;
