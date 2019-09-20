ce9f94ae55e1105026d57a60a0ffd22245423afa
tanisha2398 <tanishanegipro@gmail.com>
2019-09-20 20:50:16 +0530

    disable and enable order now button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -16,7 +16,6 @@ class BurgerBuilder extends Component {
   //   this.state={...}
   // }
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: null
@@ -41,7 +40,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchasable: sum > 0 });
+    return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -89,7 +88,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             price={this.props.price}
             ordered={this.purchaseHandler}
           />
