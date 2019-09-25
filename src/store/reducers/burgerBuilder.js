8904f4fe21d8819d32bd36dab422aa877507fe2c
tanisha2398 <tanishanegipro@gmail.com>
2019-09-25 11:40:43 +0530

    deploy on heroku

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -11,31 +11,37 @@ const INGREDIENT_PRICES = {
   meat: 1.3,
   bacon: 0.7
 };
+const addIngredient=(state,action)=>{
+  const updatedIngredient = {
+    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+  };
+  const updatedIngredients = updateObject(
+    state.ingredients,
+    updatedIngredient
+  );
+  const updatedState = {
+    ingredients: updatedIngredients,
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+  };
+  return updateObject(state, updatedState);
+}
+const removeIngredient=(state,action)=>{
+  const updatedIng = {
+    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+  };
+  const updatedIngs = updateObject(state.ingredients, updatedIng);
+  const updatedSt = {
+    ingredients: updatedIngs,
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+  };
+  return updateObject(state, updatedSt);
+}
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.ADD_INGREDIENTS:
-      const updatedIngredient = {
-        [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-      };
-      const updatedIngredients = updateObject(
-        state.ingredients,
-        updatedIngredient
-      );
-      const updatedState = {
-        ingredients: updatedIngredients,
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-      };
-      return updateObject(state, updatedState);
-    case actionTypes.REMOVE_INGREDIENTS:
-      const updatedIng = {
-        [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-      };
-      const updatedIngs = updateObject(state.ingredients, updatedIng);
-      const updatedSt = {
-        ingredients: updatedIngs,
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-      };
-      return updateObject(state, updatedSt);
+    case actionTypes.ADD_INGREDIENTS:return addIngredient(state,action)
+     
+    case actionTypes.REMOVE_INGREDIENTS:return removeIngredient(state,action)
+     
     case actionTypes.SET_INGREDIENTS:
       return updateObject(state, {
         ingredients: {
