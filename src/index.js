7f6ca2a4eceff1378ce1307a6f494f0dc75d1a31
tanisha2398 <tanishanegipro@gmail.com>
2019-09-26 00:09:03 +0530

    store token

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
+import authReducer from "./store/reducers/auth";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
-  order: orderReducer
+  order: orderReducer,
+  auth: authReducer
 });
 const store = createStore(
   rootReducer,
---
+++ src/store/reducers/auth.js
@@ -0,0 +1,40 @@
+import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
+const initialState = {
+  token: null,
+  userId: null,
+  error: null,
+  loading: false
+};
+
+const authStart = (state, action) => {
+  return updateObject(state, { error: null, loading: true });
+};
+const authSuccess = (state, action) => {
+  return updateObject(state, {
+    token: action.idToken,
+    userId: action.userId,
+    loading: false,
+    error: null
+  });
+};
+const authFail = (state, action) => {
+  return updateObject(state, {
+    loading: false,
+    error: action.error
+  });
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.AUTH_START:
+      return authStart(state, action);
+    case actionTypes.AUTH_SUCCESS:
+      return authSuccess(state, action);
+    case actionTypes.AUTH_FAIL:
+      return authFail(state, action);
+    default:
+      return state;
+  }
+};
+export default reducer;
