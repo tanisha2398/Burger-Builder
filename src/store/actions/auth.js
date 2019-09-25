62e4ca1fa6109f0e00e29247f0c0819d8fd14626
tanisha2398 <tanishanegipro@gmail.com>
2019-09-25 17:32:57 +0530

    add sign up fuctionality

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from "./actionTypes";
-
+import axios from "axios";
 export const authStart = () => {
   return {
     type: actionTypes.AUTH_START
@@ -23,5 +23,19 @@ export const authFail = error => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData={
+        email:email,
+        password:password,
+        returnSecureToken:true
+    }
+    axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCiuf4AViJ-HGo-061-ZPyxC9BvU9Z_57Y',authData)
+    .then(response=>{
+        console.log(response);
+        dispatch(authSuccess(response.data))
+    })
+    .catch(err=>{
+        console.log(err);
+        dispatch(authFail(err))
+    })
   };
 };
